568fc28013638bd3a7694e96b2f4ff9010aa29f1
Jinisha Patel <jinishakheni@gmail.com>
2024-04-01 14:45:56 +0200

    Task Done

---
+++ DiscordHero/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Nav from "../src/Components/Nav";
+import MainBody from "./Components/MainBody";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <MainBody />
+    </div>
+  );
+}
+
+export default App;
